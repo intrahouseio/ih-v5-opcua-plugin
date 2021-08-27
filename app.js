24edcf967d9202a07180567e6d8ecc70aef2281f
goodspeed1986 <goodspeed1986@gmail.com>
2021-08-27 16:05:55 +0300

    add write, update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  *
  */
 
-const util = require('util');
+const util = require("util");
 
 const {
   OPCUAClient,
@@ -12,50 +12,60 @@ const {
   AttributeIds,
   ClientSubscription,
   TimestampsToReturn,
-  ClientMonitoredItem,
   ClientMonitoredItemGroup,
-  DataType
-} = require('node-opcua');
+  DataType,
+} = require("node-opcua");
 
-const Scanner = require('./lib/scanner');
+const Scanner = require("./lib/scanner");
 // const client = require('./lib/fakeclient');
-
-
 
 module.exports = async function (plugin) {
   let client;
   let session;
+  let subscription;
+  let monitoredItem;
+
   const scanner = new Scanner(plugin);
-  //plugin.onCommand(async data => parseCommand(data));
-
-  const connectionStrategy = {
-    initialDelay: 1000,
-    maxRetry: 1
-  };
-
-  client = OPCUAClient.create({
-    applicationName: 'MyClient',
-    connectionStrategy,
-    securityMode: MessageSecurityMode.None,
-    securityPolicy: SecurityPolicy.None,
-    endpointMustExist: false
+  //plugin.onCommnad(async data => parseCommand(data))
+  plugin.onAct(async (data) => write(data));
+  plugin.channels.onChange(async function () {
+    monitoredItem.terminate();
+    const channels = await plugin.channels.get();
+    monitor(channels);
   });
 
-  async function main() {
-    const { host, port, use_password, userName, password } = plugin.params.data;
+  async function connect(params) {
+    const { host, port, use_password, userName, password } = params;
     const endpointUrl = `opc.tcp://${host}:${port}`;
-    // const endpointUrl = 'opc.tcp://opcuademo.sterfive.com:26543';
 
+    const connectionStrategy = {
+      initialDelay: 1000,
+      maxRetry: 3,
+    };
     try {
+      client = OPCUAClient.create({
+        applicationName: "MyClient",
+        connectionStrategy,
+        securityMode: MessageSecurityMode.None,
+        securityPolicy: SecurityPolicy.None,
+        endpointMustExist: false,
+      });
+
       client.on("backoff", (retry, delay) => {
-        plugin.log(`Backoff ", ${retry}, " next attempt in ", ${delay}, "ms"`, 0);
-        process.on('SIGTERM', () => {
-         process.exit(0);
+        plugin.log(
+          `Backoff ", ${retry}, " next attempt in ", ${delay}, "ms"`,
+          0
+        );
+        process.on("SIGTERM", () => {
+          process.exit(0);
         });
       });
 
       client.on("connection_lost", () => {
         plugin.log("Connection lost", 0);
+        process.on("SIGTERM", () => {
+          process.exit(0);
+        });
       });
 
       client.on("connection_reestablished", () => {
@@ -72,45 +82,32 @@ module.exports = async function (plugin) {
       client.on("after_reconnection", (err) => {
         plugin.log(`After Reconnection event =>", ${err}`, 0);
       });
+
       // step 1 : connect to
       await client.connect(endpointUrl);
-      plugin.log('connected !', 0);
+      plugin.log("connected !", 0);
 
       // step 2 : createSession
-      session = await client.createSession({ userName: userName, password: password });
-      
-      plugin.log('session created !', 0);
+      session = await client.createSession({
+        userName: userName,
+        password: password,
+      });
 
-      // step 3 : try write
-      /*session.write(
-        {
-          nodeId: 'ns=4;s=|var|WAGO 750-8215 PFC200 G2 4ETH CAN USB.Application.PLC_PRG.d',
-          attributeId: AttributeIds.Value,
-          value: {
-            value: {
-              dataType: DataType.String,
-              value: 'New value'
-            }
-          }
-        },
-        (err, statusCode) => {
-          if (!err) {
-            plugin.log('Write OK');
-          } else {
-            plugin.log('Write ERROR: ' + util.inspect(err) + ' statusCode=' + statusCode);
-          }
-        }
-      );*/
+      plugin.log("session created !", 0);
+    } catch (err) {
+      plugin.log("An error has occured : ", err);
+    }
+  }
 
-      // step 5: install a subscription and install a monitored item for 10 seconds
-      
-      const subscription = ClientSubscription.create(session, {
+  async function subscribe() {
+    try {
+      subscription = ClientSubscription.create(session, {
         requestedPublishingInterval: 1000,
         requestedLifetimeCount: 100,
         requestedMaxKeepAliveCount: 10,
         maxNotificationsPerPublish: 100,
         publishingEnabled: true,
-        priority: 10
+        priority: 10,
       });
 
       subscription
@@ -126,52 +123,79 @@ module.exports = async function (plugin) {
         .on("terminated", () => {
           plugin.log("terminated");
         });
-      
-      // install monitored item
+    } catch (err) {
+      plugin.log("An error has occured : ", err);
+    }
+  }
 
-    /*  const itemsToMonitor = [
-        {
-            nodeId: "ns=4;s=|var|WAGO 750-8212 PFC200 G2 2ETH RS Tele T.Application.PLC_PRG.a",
-            attributeId: AttributeIds.Value
-        },
+  async function monitor(channels) {
+    const itemsToMonitor = channels.map((channel) => {
+      return { nodeId: channel.id, attributeId: AttributeIds.Value };
+    });
 
-      ];*/
-      const itemsToMonitor = plugin.channels.data.map((channel)=>{
-        return {nodeId: channel.id, attributeId: AttributeIds.Value}
-      })
-
-      const parameters = {
-        samplingInterval: 100,
-        discardOldest: true,
-        queueSize: 10
-      };
-      
-      const monitoredItem = ClientMonitoredItemGroup.create(
+    const parameters = {
+      samplingInterval: 100,
+      discardOldest: true,
+      queueSize: 10,
+    };
+    try {
+      monitoredItem = ClientMonitoredItemGroup.create(
         subscription,
         itemsToMonitor,
         parameters,
         TimestampsToReturn.Both
       );
-      
+
       monitoredItem.on("changed", (monitorItem, dataValue) => {
-        let chanId = "ns=" + monitorItem.itemToMonitor.nodeId.namespace +";s=" + monitorItem.itemToMonitor.nodeId.value;
+        let chanId =
+          "ns=" +
+          monitorItem.itemToMonitor.nodeId.namespace +
+          ";s=" +
+          monitorItem.itemToMonitor.nodeId.value;
         plugin.sendData([{ id: chanId, value: dataValue.value.value }]);
         //console.log(" value has changed : ", chanId, "  ", dataValue.value.value);
       });
-      
-      
     } catch (err) {
-      plugin.log('An error has occured : ', err);
+      plugin.log("An error has occured : ", err);
     }
   }
 
-
+  async function write(data) {
+    data.data.forEach((element) =>
+      session.write(
+        {
+          nodeId: element.id,
+          attributeId: AttributeIds.Value,
+          value: {
+            value: {
+              dataType: DataType[element.dataType],
+              value: element.value,
+            },
+          },
+        },
+        (err, statusCode) => {
+          if (!err) {
+            plugin.log("Write OK");
+          } else {
+            plugin.log(
+              "Write ERROR: " + util.inspect(err) + " statusCode=" + statusCode
+            );
+          }
+        }
+      )
+    );
+  }
+  async function main() {
+    await connect(plugin.params.data);
+    await subscribe();
+    await monitor(plugin.channels.data);
+  }
 
   main();
 
   // --- События плагина ---
   // Сканирование
-  plugin.onScan(scanObj => {
+  plugin.onScan((scanObj) => {
     if (!scanObj) return;
     if (scanObj.stop) {
       //
@@ -180,12 +204,11 @@ module.exports = async function (plugin) {
     }
   });
 
-  process.on('SIGTERM', () => {
+  process.on("SIGTERM", () => {
     process.exit(0);
   });
 };
 
 async function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
