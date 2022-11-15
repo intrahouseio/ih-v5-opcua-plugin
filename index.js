02ab52e726c8fa0ed1832f9aefcb1022bf88f2b6
nimargolis <486016@gmail.com>
2022-11-15 16:41:22 +0300

    Update for binexe

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 /**
- * s7plc index.js
+ * opcua index.js
  */
 const util = require('util');
 
-const plugin = require('ih-plugin-api')();
+// const plugin = require('ih-plugin-api')();
 const app = require('./app');
 
-(async () => {
-  plugin.log('Plugin opcua client has started.', 0);
-
+(async () => {  
+  let plugin;
   try {
+    const opt = getOptFromArgs();
+    const pluginapi = opt && opt.pluginapi ? opt.pluginapi : 'ih-plugin-api';
+    plugin = require(pluginapi+'/index.js')();
+    
+    plugin.log('Plugin opcua client has started.', 0);
+
     // Получить параметры 
     plugin.params.data = await plugin.params.get();
     plugin.log('Received params data:'+util.inspect(plugin.params.data));
@@ -23,3 +28,13 @@ const app = require('./app');
     plugin.exit(8, `Error: ${util.inspect(err)}`);
   }
 })();
+
+function getOptFromArgs() {
+  let opt;
+  try {
+    opt = JSON.parse(process.argv[2]); //
+  } catch (e) {
+    opt = {};
+  }
+  return opt;
+}
